a5c2e7bb9a733f8e05866534bec2bf5969f43e72
wa-vc <admin@worldanchor.com>
2022-03-26 14:40:47 -0500

    Added basic car tracking with camera

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -8,6 +8,7 @@ public class GameManager : MonoBehaviour
     {
         // Spawn the local player
         Ref.I.localPlayer = Instantiate(Ref.I.playerPrefab, new Vector3(0, 5, 0), new Quaternion(), Ref.I.world.transform);
-
+        // Make camera follow player
+        Ref.I.virtualCamera1.Follow = Ref.I.localPlayer.transform;
     }
 }
